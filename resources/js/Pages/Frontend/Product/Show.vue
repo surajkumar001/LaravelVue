e48d6bed827de322f624d4f65e2f49ac81e2ad46
Suraj Kumar <surajprince01@gmail.com>
2024-08-02 14:19:30 +0530

    show page change

--- resources/js/Pages/Frontend/Product/Show.vue
+++ resources/js/Pages/Frontend/Product/Show.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 bg-gray-100">
 
-    <div class="col-span-6">
+    <div v-if="product.image" class="col-span-6">
         <img :src="`/storage/${product.image}`" alt="Uploaded Image">
     </div>
     <div class="col-span-6">
